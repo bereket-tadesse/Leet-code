404772d05a0a38965b34bf2b9e2869db4e677019
Bereket Tadesse <bekimehari@gmail.com>
2023-06-09 01:06:32 -0400

    update

---
+++ Problem 1/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        // key, value -> target - nums[i] , i
+        Map<Integer, Integer> complements = new HashMap();
+
+        for (int i = 0; i < nums.length; i++){
+
+            Integer complements_index = complements.get(nums[i]); //get returns the value which is index e
+            if (complements_index != null) {
+                return new int[] {i, complements_index};
+            } else {
+                complements.put(target - nums[i], i);
+            }
+        }
+        return nums;
+    }
+}
